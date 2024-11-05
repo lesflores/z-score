4e4f53987a1d24640c3278e26445cb6b78618c92
Lesly Estefanía Flores Rivera <147342430+lesflores@users.noreply.github.com>
2024-11-04 23:00:09 -0600

    Update z-score.R

--- z-score.R
+++ z-score.R
@@ -1,7 +1,6 @@
-# Quieres que tus variables sean comparables?
-
-# Normalización z-score: 
-# Convierte los datos a una media de 0 y una desviación estándar de 1.
+#---------- Quieres que tus variables sean comparables?
+#---------- Normalización z-score: 
+#---------- Convierte los datos a una media de 0 y una desviación estándar de 1.
 
 # "datos" es el nombre de la base de datos 
 # "variable" es el nombre de tu variable en cada columna
@@ -11,27 +10,28 @@ datos$variable_estandarizada2 <- scale(datos$variable2)
 
 # Ejemplo:
 
-# llamamos base de datos de prueba que puedes descargar aquí https://github.com/lesflores/z-score/blob/main/datos.xlsx
+#---------- llamamos base de datos de prueba que puedes descargar aquí https://github.com/lesflores/z-score/blob/main/datos.xlsx
 
 library(readxl)
 datos <- read_excel("datos.xlsx") 
 
-head(datos) # Al ejecutar, verás los primeros datos y nombres de las columnas
+#---------- Al ejecutar "head", verás los primeros datos y nombres de las columnas
+head(datos) 
 
-# Ejemplo de datos iniciales:
 # indiceFelicidad   satisfaccionDemocracia
 # <dbl>             <dbl>
 # 1                 60                7
 # 2                 80                6
 # 3                 70                8
 
-# Aplicamos la normalización z-score
+#---------- Aplicamos la normalización z-score
+
 datos$indiceFelicidad_estandarizada <- scale(datos$indiceFelicidad)
 datos$satisfaccionDemocracia_estandarizada <- scale(datos$satisfaccionDemocracia)
 
-head(datos) # Al volver a ejecutar, veremos los datos con las variables estandarizadas
+#---------- # Al volver a ejecutar "head", verás los datos con las variables estandarizadas
+head(datos)
 
-# Ejemplo de salida:
 # indiceFelicidad   satisfaccionDemocracia   indiceFelicidad_estandarizada   satisfaccionDemocracia_estandarizada
 # <dbl>             <dbl>                   <dbl>                           <dbl>
 # 1                 60                      7                              -0.523                            0.152
